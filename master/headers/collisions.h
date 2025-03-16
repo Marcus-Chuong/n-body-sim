9c3d39dbc51fd0bf1ad0f1b13c14ddcc59a3582b
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2025-03-16 17:27:34 -0400

    made cillision file

---
+++ master/headers/collisions.h
@@ -0,0 +1,6 @@
+#include <vector>
+#include "C:\Users\marcu\Desktop\Folder of Death\n-body-sim\master\headers\vector.h"
+#include "C:\Users\marcu\Desktop\Folder of Death\n-body-sim\master\headers\particle.h"
+#include "C:\Users\marcu\Desktop\Folder of Death\n-body-sim\master\headers\quadtree.h"
+
+void collisionWithParticle();
