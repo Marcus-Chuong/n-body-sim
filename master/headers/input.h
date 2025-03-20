61f402a9125b179dcbad95a5cb6c4108c07478f2
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2025-03-19 21:11:04 -0400

    making input files

---
+++ master/headers/input.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void mouseDragged();
+void mousePressed();
+void mouseReleased();
+void mouseWheel(MouseEvent event);
+void inputLoop();
---
+++ master/src/input.cpp
@@ -0,0 +1,39 @@
+#include "C:\Users\marcu\Desktop\Folder of Death\n-body-sim\master\headers\input.h"
+
+void mouseDragged() {
+  if (mouseButton == RIGHT) { updateCam(); return; }
+}
+
+void mousePressed() {
+  if (mouseButton == RIGHT) { setCamOffsets(); return; }
+  if (mouseButton == LEFT) { setInsertStart(); return; }
+}
+
+void mouseReleased() {
+  if (mouseButton == LEFT) { insertSquare(); return; }
+}
+
+void mouseWheel(MouseEvent event) {
+  float e = event.getCount();
+
+  if (e < 0) {
+    tox -= dzoom * (mouseX - tox);
+    toy -= dzoom * (mouseY - toy);
+    tozoom *= dzoom + 1;
+
+  } else {
+    tox += dzoom / (dzoom + 1) * (mouseX - tox); 
+    toy += dzoom / (dzoom + 1) * (mouseY - toy);
+    tozoom /= dzoom + 1;
+  }
+}
+
+void inputLoop() {
+  /*
+  if (mousePressed) {
+    if (mouseButton == LEFT && frameCount % everyInsert == 0) {
+      insertSquare(screenToSpace(mouseX, mouseY), 20f);
+    }
+  }
+  */
+}
